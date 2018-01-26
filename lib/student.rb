e1bd68f7476cefb0f9ad01793b693eb630531773
John Wojciechowski <jwojo13@gmail.com>
2018-01-26 18:14:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student_hash|
-      person = Student.new(person_hash)
+      person = Student.new(student_hash)
     end
   end
 
