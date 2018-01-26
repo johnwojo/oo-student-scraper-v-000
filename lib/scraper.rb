04843f0ed495d2999d758789c6b75795d02f7d44
John Wojciechowski <jwojo13@gmail.com>
2018-01-26 16:43:04 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ class Scraper
    def self.scrape_profile_page(profile_url)
     html = open('fixtures/student-site/index.html')
     profile = Nokogiri::HTML(html)
-    doc.css('')
+    links = profile.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value})
   end
 
 end
