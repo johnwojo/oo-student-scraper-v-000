045a46f6ac8d7339c65553e3508962030f1105dd
John Wojciechowski <jwojo13@gmail.com>
2018-01-26 16:38:38 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,11 +20,11 @@ class Scraper
  end
 
 
+   def self.scrape_profile_page(profile_url)
     html = open('fixtures/student-site/index.html')
     doc = Nokogiri::HTML(html)
   end
 
-  def self.scrape_profile_page(profile_url)
 
   end
 
