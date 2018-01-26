9c930dbcc12094e6813f2ff7a7db9e5d8057d7aa
John Wojciechowski <jwojo13@gmail.com>
2018-01-26 18:18:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |key, value|
+    attributes_hash.each do |attr, value|
       self.send("#{key}",value)
     end
     self
