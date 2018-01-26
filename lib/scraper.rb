0c5dea30b4e3510e578f9cb27fb6554fc37b1637
John Wojciechowski <jwojo13@gmail.com>
2018-01-26 17:38:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
     html = open(profile_url)
     profile = Nokogiri::HTML(html)
     student = {}
-    links = profile.css(".social-icon-container a").each do |link|
+    profile.css(".social-icon-container a").each do |link|
       link_attribute = link.attribute("href").value
       if link_attribute.include?('twitter')
         student[:twitter] = link
